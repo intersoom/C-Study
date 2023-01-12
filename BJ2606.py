733870aa0a4ee3d63953e196c368b7c919ef106d
intersoom <sallybig@naver.com>
2023-01-12 20:37:41 +0900

    백준 2606번 (인접 리스트 방식 사용)

--- BJ2606.py
+++ BJ2606.py
@@ -1,4 +1,4 @@
-# 인접행렬 방식 사용
+# # 인접행렬 방식 사용
 computerNum = int(input())
 linkNum = int(input())
 
@@ -35,3 +35,37 @@ while len(queue) > 0:
 
 print(len(used) - 1)
 
+###############################################################
+# 인접리스트 방식 사용
+computerNum = int(input())
+linkNum = int(input())
+
+arr = []
+queue = [1]
+used = []
+tempArr = []
+
+for i in range(computerNum + 1):
+    arr.append([])
+
+ab = []
+
+for i in range(linkNum):
+    ab = input().split(" ")
+    a = int(ab[0])
+    b = int(ab[1])
+
+    arr[a].append(b)
+    arr[b].append(a)
+
+while len(queue) > 0:
+    index = queue[0]
+    pop = queue.pop(0)
+    if pop not in used:
+        used.append(pop)
+
+    for i in arr[index]:
+        if i not in used:
+            queue.append(i)
+
+print(len(used) - 1)
