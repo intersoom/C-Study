ae21bffa46d2a6d3758a24504871febdc254ae84
intersoom <sallybig@naver.com>
2023-07-19 13:59:14 +0900

    백준 1027번

---
+++ BJ1027.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+
+heap = []
+curArr = []
+
+
+N = int(sys.stdin.readline())
+
+for i in range(N):
+    cur = int(sys.stdin.readline())
+              
+    if cur == 0:
+      if len(heap) == 0:
+        print(0)
+      else:
+        print(heapq.heappop(heap))
+    else:
+      heapq.heappush(heap, cur)
