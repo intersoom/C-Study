a1e27dda5b41e81eae1f2c793651dde417326f9e
intersoom <sallybig@naver.com>
2023-08-23 20:33:46 +0900

    백준 1363번

--- BJ1463.py
+++ BJ1463.py
@@ -1,20 +1,37 @@
-user = int(input())
-count = 0
+import sys
+sys.setrecursionlimit(10**7)
 
-def rule(x):
-    print(x)
-    if x % 3 == 1:
-        return x - 1
-    elif x % 2 == 0:
-        return x // 2
-    else:
-        return x - 1
+userInput = int(input())
 
-hello = int(user)
+dp = [0] * (userInput + 1)
 
-while hello > 1:
-    count += 1
-    hello = rule(hello)
+for i in range(2, userInput + 1):
+    dp[i] = dp[i - 1] + 1
 
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
 
-print(count)
+print(dp[userInput])
+
+#### top-bottom으로는 못 푸나..
+
+# def solve(x):
+#     if x == 1:
+#         return 0
+    
+#     if dp[x] != 0:
+#         return dp[x]
+    
+#     dp[x] = solve(x - 1) + 1
+
+#     if x % 2 == 0:
+#         dp[x] = min(dp[x], solve(x // 2) + 1)
+    
+#     if x % 3 == 0:
+#         dp[x] = min(dp[x], solve(x // 3) + 1)
+
+#     return dp[x]
+
+# print(solve(userInput))
