1abce99887225ee8a63ab1f380bf0f8740ad7939
intersoom <sallybig@naver.com>
2023-08-27 23:50:11 +0900

    백준 7568번

--- BJ7568.py
+++ BJ7568.py
@@ -6,6 +6,7 @@ result = []
 start = 0
 end = 0
 
+
 for i in range(N):
   temp = sys.stdin.readline().split(' ')
   temp[0] = int(temp[0])
@@ -13,35 +14,24 @@ for i in range(N):
 
   arr.append(temp)
 
-arr1 = sorted(arr, key=lambda x: -x[0])
+# 몸무게로 정렬
+arr1 = sorted(arr, key=lambda x: (-x[0], -x[1]))
+# 키로 정렬
 arr2 = sorted(arr, key=lambda x: -x[1])
-print(arr)
 
-for i in range(len(arr1)):
-  if start == 0 and arr1[i] != arr2[i]:
-    start = i
-  elif arr1[i] != arr2[i]:
-    end = i
+count = 1
 
-for i in range(0, start):
-  arr1[i].append(i + 1)
-  result.append(arr1[i])
-
-for i in range(start, end + 1):
-  arr1[i].append(start + 1)
-  result.append(arr1[i])
-
-for i in range(end + 1, len(arr1)):
-  arr1[i].append(i + 1)
-  result.append(arr1[i])
+# arr1[0].append(1)
 
 
-for i in arr:
-  for j in result:
-    if i[0] == j[0] and i[1] == j[1]:
-      if j == result[N - 1]:
-        print(j[2])
-      else:
-        print(j[2], end=' ')
-      
-      break
+
+print(arr1)
+print(arr2)
+ 
+# for i in arr:
+#   count += 1
+#   if count == N:
+#     print(i[2])
+#   else:
+#     print(i[2], end=' ')
+  
