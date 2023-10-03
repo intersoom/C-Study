6b56b50401214c8ddc806ad647956b2083b265b7
intersoom <sallybig@naver.com>
2023-10-03 17:25:14 +0900

    백준 1011번

--- BJ1011.py
+++ BJ1011.py
@@ -1,13 +1,16 @@
-import sys
-  
-TC = int(sys.stdin.readline())
+### 내가 푼거 아님 ((공부완료!))
 
-for i in range(TC):
-  xy = sys.stdin.readline().split(' ')
-  x = int(xy[0])
-  y = int(xy[1])
+t = int(input())
 
-  if (y - x) % 2 == 0:
-    print((y - x) // 2 + 1)
-  else:
-    print((y - x) // 2 + 2)
+for _ in range(t):
+    x, y = map(int,input().split())
+    distance = y - x
+    count = 0  # 이동 횟수
+    move = 1  # count별 이동 가능한 거리
+    move_plus = 0  # 이동한 거리의 합
+    while move_plus < distance :
+        count += 1
+        move_plus += move  # count 수에 해당하는 move를 더함
+        if count % 2 == 0 :  # count가 2의 배수일 때, 
+            move += 1  
+    print(count)
